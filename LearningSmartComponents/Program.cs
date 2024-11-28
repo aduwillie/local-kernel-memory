4ec6a7a81e448478e107d9d219e7414805e56ec6
Williams Adu <aduwillie@gmail.com>
2024-11-28 02:40:38 -0800

    Initial commit

---
+++ LearningSmartComponents/Program.cs
@@ -0,0 +1,62 @@
+﻿#pragma warning disable SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Embeddings;
+using SmartComponents.LocalEmbeddings;
+
+var embedder = new LocalEmbedder();
+
+var cat = embedder.Embed("Cats can be blue.");
+var dog = embedder.Embed("Dogs can be red.");
+var random = embedder.Embed("This is something very random.");
+
+// Comparing things that are similar
+var kitten = embedder.Embed("Kittens!!!");
+Console.WriteLine(cat.Similarity(kitten));
+
+// Comparing non-similar things
+Console.WriteLine(cat.Similarity(random));
+Console.WriteLine(dog.Similarity(random));
+
+// Semantic Kernel setup
+var kernel = Kernel.CreateBuilder()
+  .AddLocalTextEmbeddingGeneration()
+  .Build();
+
+var embeddingsGenerator = kernel.GetRequiredService<ITextEmbeddingGenerationService>();
+var inputEmbedding = await embeddingsGenerator
+  .GenerateEmbeddingAsync("Williams is a Semantic Kernel developer.");
+
+Console.WriteLine($"Input embedding length: {inputEmbedding.Length}");
+
+var info = new[]
+{
+  "Williams is married to Abigail",
+  "Williams works at Microsoft",
+  "Another nickname of Williams is Bill"
+};
+var infoEmbeddings = info.Select(i => new Info
+{
+  Text = i,
+  Embedding = embedder.Embed(i),
+});
+
+var question = "Where does Williams work?";
+var questionEmbedding = embedder.Embed(question);
+
+var answers = LocalEmbedder.FindClosest(
+  target: questionEmbedding,
+  candidates: infoEmbeddings.Select(i => (i, i.Embedding)),
+  maxResults: 1);
+
+Console.WriteLine();
+Console.WriteLine($"Question: {question}");
+Console.WriteLine($"Answer: {answers.FirstOrDefault()?.Text ?? "No answer for you."}");
+
+Console.ReadLine();
+
+class Info
+{
+  public string Text { get; init; } = string.Empty;
+  public EmbeddingF32 Embedding { get; init; }
+}
---
+++ SKLocalRag/Program.cs
@@ -0,0 +1,117 @@
+﻿#pragma warning disable SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+using feiyun0112.SemanticKernel.Connectors.OnnxRuntimeGenAI;
+using Microsoft.KernelMemory;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Embeddings;
+using Microsoft.SemanticKernel.TextGeneration;
+using SmartComponents.LocalEmbeddings;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+var skConfig = new Microsoft.KernelMemory.SemanticKernel.SemanticKernelConfig();
+
+var memory = new KernelMemoryBuilder()
+  .WithSemanticKernelTextEmbeddingGenerationService(
+    service: new CustomTextEmbeddingService(),
+    config: skConfig)
+  .WithSemanticKernelTextGenerationService(
+    service: new CustomTextGenerationService(),
+    config: skConfig)
+  .WithDefaultWebScraper()
+  .Build();
+
+//var aliceInWonderLandUri = "https://www.gutenberg.org/files/11/11-h/11-h.htm";
+//await memory.ImportWebPageAsync(aliceInWonderLandUri);
+await memory.ImportTextAsync("""
+At the beginning of the book, Alice was jumping. She followed the rabbit because she was hungry.
+""");
+
+var question1 = "What was Alice doing with her sister at the beginning of the book?";
+Console.WriteLine($"Question: {question1}");
+Console.WriteLine($"Answer: {await memory.AskAsync(question1)}");
+
+var question2 = "Why did Alice follow the White Rabbit?";
+Console.WriteLine($"Question: {question2}");
+Console.WriteLine($"Answer: {await memory.AskAsync(question2)}");
+
+Console.ReadLine();
+
+public class CustomTextEmbeddingService() : ITextEmbeddingGenerationService
+{
+  private readonly Dictionary<string, object?> _internalAttributes = [];
+  private readonly LocalEmbedder embedder = new LocalEmbedder();
+
+  public IReadOnlyDictionary<string, object?> Attributes => _internalAttributes;
+
+  public Task<IList<ReadOnlyMemory<float>>> GenerateEmbeddingsAsync(IList<string> data, Kernel? kernel = null, CancellationToken cancellationToken = default)
+  {
+    IList<ReadOnlyMemory<float>> embeddings = data
+      .Select(d => embedder.Embed(d).Values).ToList() ?? new();
+
+    return Task.FromResult(embeddings);
+  }
+}
+
+class CustomTextGenerationService : ITextGenerationService
+{
+  private string modelPath;
+  private Kernel baseKernel;
+
+  public IReadOnlyDictionary<string, object?> Attributes => new Dictionary<string, object?>();
+
+  public CustomTextGenerationService()
+  {
+    var path = Path.Combine(
+      Directory.GetParent(Assembly.GetExecutingAssembly().Location)!.FullName,
+      "Models",
+      "Phi-3-medium-4k-instruct-onnx-cpu",
+      "cpu-int4-rtn-block-32-acc-level-4");
+    if (!Directory.Exists(path))
+    {
+      throw new DirectoryNotFoundException($"Model file not found at {path}");
+    }
+
+    modelPath = path;
+    baseKernel = Kernel.CreateBuilder()
+      .AddLocalTextEmbeddingGeneration()
+      .AddOnnxRuntimeGenAIChatCompletion(
+        modelPath: modelPath)
+      .Build();
+
+    var answer = baseKernel.InvokePromptAsync("Hello", new KernelArguments
+    (
+      new OnnxRuntimeGenAIPromptExecutionSettings { MaxLength = 2048 }
+    )).Result;
+    Console.WriteLine(answer.ToString());
+  }
+
+  public async IAsyncEnumerable<StreamingTextContent> GetStreamingTextContentsAsync(string prompt, PromptExecutionSettings? executionSettings = null, Kernel? kernel = null, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+  {
+    await foreach(var textContent in RunPromptWithStreaming(prompt, null, cancellationToken))
+    {
+      yield return textContent;
+    }
+  }
+
+  public Task<IReadOnlyList<TextContent>> GetTextContentsAsync(string prompt, PromptExecutionSettings? executionSettings = null, Kernel? kernel = null, CancellationToken cancellationToken = default)
+  {
+    return Task.FromResult<IReadOnlyList<TextContent>>([]);
+  }
+
+  private async Task<string> RunPromptWithoutStreaming(string prompt, KernelArguments? args, CancellationToken cancellationToken)
+  {
+    var result = await baseKernel.InvokePromptAsync(prompt, args, cancellationToken: cancellationToken);
+    return result.ToString();
+  }
+
+  private async IAsyncEnumerable<StreamingTextContent> RunPromptWithStreaming(string prompt, KernelArguments? args, [EnumeratorCancellation] CancellationToken cancellationToken)
+  {
+    var streamResults = baseKernel.InvokePromptStreamingAsync(prompt, args, cancellationToken: cancellationToken);
+    await foreach (var content in streamResults)
+    {
+      var textContent = content.ToString();
+      yield return new StreamingTextContent(textContent);
+    }
+  }
+}
